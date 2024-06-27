b87e9046d720b8741dc886c4c5e7884a7251f662
NikitaAsmedianov <nikitaa@campus.technion.ac.il>
2024-06-27 14:35:05 +0300

    Report Comsol

--- XXRapid_Qt_viewer/ExperimentQMainWindow/ExperimentQWidget/ComsolSimulationQTabWidget/CAIComsolQTabWidget/Graphics.py
+++ XXRapid_Qt_viewer/ExperimentQMainWindow/ExperimentQWidget/ComsolSimulationQTabWidget/CAIComsolQTabWidget/Graphics.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from MPLQWidgets.MatplotlibSingeAxQWidget import *
 
 
@@ -5,19 +7,25 @@ class Graphics(MatplotlibSingeAxQWidget):
     def __init__(self, parent):
         self.parent = parent
         self.CAI_dict = self.parent.current_dict
+        self.settings_key = self.parent.current_key
+        self.report_path = self.parent.report_path
         super().__init__()
         self.ax.set(
             ylabel='$h_{edge} \\times 10^{9} A^2s/cm^4$',
             xlabel='$J_{edge} \\times 10^{7} A/cm^2$'
         )
         self.error_bar = self.ax.errorbar(
-            np.array(self.CAI_dict['j_exp'])*1e-7,
-            np.array(self.CAI_dict['h_exp'])*1e-9,
-            xerr=np.array(self.CAI_dict['j_exp'])*1e-7 * 0.15,
-            yerr=np.array(self.CAI_dict['h_exp'])*1e-9 * 0.25,
+            np.array(self.CAI_dict['j_exp']) * 1e-7,
+            np.array(self.CAI_dict['h_exp']) * 1e-9,
+            xerr=np.array(self.CAI_dict['j_exp']) * 1e-7 * 0.15,
+            yerr=np.array(self.CAI_dict['h_exp']) * 1e-9 * 0.25,
             fmt='o'
 
         )
 
     def refresh(self):
         self.tof_dict = self.parent.tof_dict
+
+    def save_report(self):
+        self.figure.savefig(f'{self.report_path}/{self.settings_key}.png')
+        pd.DataFrame(self.CAI_dict).to_csv(f'{self.report_path}/{self.settings_key}.csv')
--- XXRapid_Qt_viewer/ExperimentQMainWindow/ExperimentQWidget/ComsolSimulationQTabWidget/CAIComsolQTabWidget/__init__.py
+++ XXRapid_Qt_viewer/ExperimentQMainWindow/ExperimentQWidget/ComsolSimulationQTabWidget/CAIComsolQTabWidget/__init__.py
@@ -7,6 +7,7 @@ from SettingsQWidgets.ChildQTabWidget import *
 class CAIComsolQTabWidget(ChildQTabWidget):
     def __init__(self, parent):
         super().__init__(parent, 'CAI_Comsol')
+        self.report_path = f'{self.parent.report_path}/{self.settings_key}'
         self.ComsolCurrentQTabWidget = self.parent.ComsolCurrentQTabWidget
         self.CAI_dict = self.ComsolCurrentQTabWidget.CAI_dict
         self.Graphics_dict = dict()
@@ -16,5 +17,13 @@ class CAIComsolQTabWidget(ChildQTabWidget):
             try:
                 self.Graphics_dict[my_key] = Graphics(self)
                 self.addTab(self.Graphics_dict[my_key], my_key)
+            except Exception as ex:
+                print(ex)
+
+    def save_report(self):
+        os.makedirs(self.report_path, exist_ok=True)
+        for graphics in self.Graphics_dict.values():
+            try:
+                graphics.save_report()
             except Exception as ex:
                 print(ex)
